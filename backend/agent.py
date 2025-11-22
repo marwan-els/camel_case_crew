8e780409a2e835184b1d932a19a0d36be8239d93
Marwan Elsayed <ge27zec@mytum.de>
2025-11-22 12:51:23 +0100

    Add initial agent

---
+++ backend/agent.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+from elevenlabs.client import ElevenLabs
+import os
+from pathlib import Path
+
+
+load_dotenv()
+
+elevenlabs = ElevenLabs(
+    api_key=os.getenv("ELEVENLABS_API_KEY"),
+)
+
+prompt = Path(__file__).with_name("sixt_agent_prompt.txt").read_text()
+
+
+response = elevenlabs.conversational_ai.agents.create(
+    name="My voice agent",
+    tags=["test"], 
+    conversation_config={
+        "tts": {
+            "voice_id": "21m00Tcm4TlvDq8ikWAM",
+            "model_id": "eleven_flash_v2"
+        },
+        "agent": {
+            "first_message": "Hi, this is Rachel from SixT. How can I help you today?",
+            "prompt": {
+                "prompt": prompt,
+            }
+        }
+    }
+)
+
+print("Agent created with ID:", response.agent_id)
